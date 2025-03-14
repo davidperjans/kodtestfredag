7a9f4d6146cec3c5a3e23a381925c6f12841ee02
davidperjans <perjansdavid@gmail.com>
2025-03-14 10:51:37 +0100

    added method to find first non repeatable character

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FirstNonRepeatableChar("swiss")); // w
+            Console.WriteLine(FirstNonRepeatableChar("racecars")); // e
+            Console.WriteLine(FirstNonRepeatableChar("aabb")); // _
+        }
+
+        public static char FirstNonRepeatableChar(string text)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (var character in text)
+            {
+                if (charCount.ContainsKey(character))
+                {
+                    charCount[character]++;
+                }
+                else
+                {
+                    charCount.Add(character, 1);
+                }
+            }
+
+            foreach (var character in text)
+            {
+                if (charCount[character] == 1)
+                {
+                    return character;
+                }
+            }
+
+            return '_';
         }
     }
 }
